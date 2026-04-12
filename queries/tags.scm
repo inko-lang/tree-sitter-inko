a843f42f51ebfb4ffcf776bab28c337dda3f33af
Yorick Peterse <yorick@yorickpeterse.com>
2026-04-12 23:07:12 +0200

    Remove queries/ directory
    As far as I know not a single tool out there actually uses the queries/
    directory as-is, as every tool has its own capture groups and what not.
    In addition, the queries bundled haven't been updated for a while
    anyway.
    
    Changelog: changed

--- queries/tags.scm
+++
@@ -1,4 +0,0 @@
-(method name: _ @name) @definition.function
-(external_function name: _ @name) @definition.function
-(class name: _ @name) @definition.class
-(trait name: _ @name) @definition.interface
